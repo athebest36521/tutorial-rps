d9bdda68ceec994917c9d21bbeb784865fce0e72
athebest36521 <526atiwari@frhsd.com>
2025-01-22 11:48:09 -0500

    not much...ADDED SO MUCH MORE AT HOME! NOT SHOWING UP!!!!!!!!!!

--- script-Tiwari.js
+++ script-Tiwari.js
@@ -2,8 +2,12 @@ console.log("hi");
 // Function to get the user's choice
 // Function to get the user's choice
 // Function to get the user's choice
-
+var username=prompt('What is your name?');
+document.getElementById('username').innerHTML='Hey '+username+ ', Let\'s get this game started';
+//Hey '(username)' Let's get started
 // Function to get the computer's choice
+
+
 const getComputerChoice = () => {
   const randomNumber = Math.floor(Math.random() * 3);
   switch (randomNumber) {
@@ -18,9 +22,6 @@ const getComputerChoice = () => {
 
 // Function to determine the winner
 const determineWinner = (userChoice, computerChoice) => {
-  if (userChoice === 'bomb') {
-    return 'You won!';
-  }
   if (userChoice === computerChoice) {
     return 'The game is a tie!';
   }
@@ -34,21 +35,23 @@ const determineWinner = (userChoice, computerChoice) => {
     return computerChoice === 'rock' ? 'The computer won!' : 'You won!';
   }
 };
-
+var userwins=0;
+var computerwins=0;
 // Function to play the game
-const playGame = (userChoice) => {
+const playGame = (event, userChoice) => {
+  event.preventDefault()
   const computerChoice = getComputerChoice();
-  console.log('You threw: ' + userChoice);
-  console.log('The computer threw: ' + computerChoice);
+  document.write('You threw: ' + userChoice);
+  document.write('The computer threw: ' + computerChoice);
 
-  console.log(determineWinner(userChoice, computerChoice));
+  document.write(determineWinner(userChoice, computerChoice));
 };
- const resetGame = () => {
-     userScore = 0;
-     computerScore = 0;
-     round = 0;
-     document.getElementById('game-result').innerHTML = '';
-     document.getElementById('result').innerHTML = '';
-     document.getElementById('computer-choice').style.backgroundImage = '';
-   };
-
+// const resetGame = () => {
+//     userScore = 0;
+//     computerScore = 0;
+//     round = 0;
+//     document.getElementById('game-result').innerHTML = '';
+//     document.getElementById('result').innerHTML = '';
+//     document.getElementById('computer-choice').style.backgroundImage = '';
+//   };
+  
